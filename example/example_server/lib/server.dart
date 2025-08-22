039fb594a262dd5a4a6edfcb6471bb03add53326
Jei-sKappa <guzzojacopo95@gmail.com>
2025-08-22 19:42:37 +0200

    refactor: update server

--- example/example_server/lib/server.dart
+++ example/example_server/lib/server.dart
@@ -1,4 +1,3 @@
-import 'package:example_server/src/crdt_store.dart';
 import 'package:serverpod/serverpod.dart';
 
 import 'src/generated/protocol.dart';
@@ -6,20 +5,6 @@ import 'src/generated/endpoints.dart';
 
 /// The starting point of the Serverpod server.
 void run(List<String> args) async {
-  final crdt = await createCrdt();
-  crdt.onTablesChanged.listen(
-    (event) async {
-      print('SERVER:');
-      final all = await crdt.getChangeset();
-      for (final entry in all.entries) {
-        print('  ${entry.key}:');
-        for (final record in entry.value) {
-          print('    ${record['id']}: ${record['message']}');
-        }
-      }
-    },
-  );
-
   // Initialize Serverpod and connect it with your generated code.
   final pod = Serverpod(args, Protocol(), Endpoints());
 
